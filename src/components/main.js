5203f715b5631f18770ec324609ca1a2e1777521
AlexPurhalo <alexpurhalo@gmail.com>
2016-12-17 21:32:22 +0200

    activity-log-section-styling

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,15 @@
-
 // Node modules import
 import React, { Component } from 'react'
 
+// Components import
+import ActivityLog from './main/activity-log';
 // Renders main page of application
 export default class Main extends Component {
 	render() {
-		return <div className="main-page"></div>;
+		return (
+			<div className="main-page">
+				<ActivityLog />
+			</div>
+		);
 	}
 }
